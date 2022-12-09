95b1a8c8374a9cec69ea41605de1bfcac129163a
ProgrammingColumbus <103267295+ProgrammingColumbus@users.noreply.github.com>
2022-12-09 10:41:52 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+//Start
+
+song = "";
+
+
+
+function preload(){
+    song = loadSound("music.mp3");
+    song = loadSound("music2.mp3")
+}
+
+function setup(){
+    canvas = createCanvas(500, 400);
+    canvas.position(400, 200);
+
+    video=createCapture(VIDEO);
+    video.hide();
+}
+
+function draw(){
+    image(video, 0, 0, 500, 400);
+}
+
+function playSound(){
+    song.play();
+}
