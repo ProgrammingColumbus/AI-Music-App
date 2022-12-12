aff0a625a0283c9507f40952e7ecd1ea5128f2fc
ProgrammingColumbus <103267295+ProgrammingColumbus@users.noreply.github.com>
2022-12-12 10:16:30 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,12 +1,15 @@
 //Start
 
 song = "";
-
+leftWristX = 0;
+leftWristY = 0;
+rightWristX = 0;
+rightWristY = 0;
 
 
 function preload(){
     song = loadSound("music.mp3");
-    song = loadSound("music2.mp3")
+    song = loadSound("music2.mp3");
 }
 
 function setup(){
@@ -15,6 +18,26 @@ function setup(){
 
     video=createCapture(VIDEO);
     video.hide();
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on('pose', gotPoses);
+}
+
+function modelLoaded(){
+    console.log("PoseNet: Initialized");
+}
+
+function gotPoses(results){
+    if(results.length > 0){
+        console.log(results);
+        leftWristX = results[0].pose.leftWrist.x;
+        leftWristY = results[0].pose.leftWrist.y;
+        rightWristX = results[0].pose.rightWrist.x;
+        rightWristY = results[0].pose.rightWrist.y;
+
+        console.log("Left Wrist X:  " + leftWristX + ", Left Wrist Y:  " + leftWristY);
+        console.log("Right Wrist X:  " + rightWristX + ", Right Wrist Y:  " + rightWristY);
+    }
 }
 
 function draw(){
@@ -24,3 +47,4 @@ function draw(){
 function playSound(){
     song.play();
 }
+
